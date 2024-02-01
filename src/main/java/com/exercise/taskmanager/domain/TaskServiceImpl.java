062dd77f9f969c5e0484365cc51b119091f67032
leonelborgesdev <leonel03t@gmail.com>
2024-01-31 23:53:53 -0400

    implement servicetaskinterface

--- src/main/java/com/exercise/taskmanager/domain/TaskServiceImpl.java
+++ src/main/java/com/exercise/taskmanager/domain/TaskServiceImpl.java
@@ -1,4 +1,29 @@
 package com.exercise.taskmanager.domain;
 
-public class TaskServiceImpl {
+import java.util.Collections;
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService {
+    private Task task;
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    @Override
+    public Task getTaskById(long id) {
+        // En una implementación real, buscaríamos la tarea por su ID de una fuente de datos, una lista o una BD
+        // Para este ejemplo, devolveremos el bean de task inyectado
+        return task;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return Collections.singletonList(task);
+    }
+
+    @Override
+    public void addTask(Task task) {
+        this.task = task;
+    }
 }
