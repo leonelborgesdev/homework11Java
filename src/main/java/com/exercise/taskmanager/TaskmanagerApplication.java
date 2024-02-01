22bd5c187c9cdd0cb63557514b4a8e2f490c1793
leonelborgesdev <leonel03t@gmail.com>
2024-01-31 23:54:40 -0400

    main taskmanager

--- src/main/java/com/exercise/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/exercise/taskmanager/TaskmanagerApplication.java
@@ -14,7 +14,28 @@ public class TaskmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
 
+	TaskService taskService = new TaskServiceImpl();
 
+	// Ejemplo 1: Obtener una tarea por su ID
+	Task task = taskService.getTaskById(1);
+		System.out.println("Tarea obtenida por ID:");
+		System.out.println(task);
+
+	// Ejemplo 2: Agregar una nueva tarea
+	Task newTask = new Task();
+		newTask.setId(2);
+		newTask.setTitle("Implement Data Persistence");
+		newTask.setDescription("Store tasks in a database.");
+	//newTask.setDueDate(LocalDate.of(2023, 9, 15));
+		taskService.addTask(newTask);
+		System.out.println("Nueva tarea agregada:");
+
+	// Ejemplo 3: Obtener todas las tareas
+	List<Task> allTasks = taskService.getAllTasks();
+		System.out.println("Todas las tareas:");
+		for (Task t : allTasks) {
+		System.out.println(t);
+	}
 	}
 
 }
